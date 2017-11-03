f6b97e7f9b1660d43f5c0318933805debe0ae1ee
SDMesaRoboticsOrganization <sdmesa.roboticsorganization@gmail.com>
2017-11-03 14:52:11 -0700

    v1.1
    This is version 1.1 of the micromouse_prototype.

--- Micromouse/prototype/code/micromouse_prototype.ino
+++ Micromouse/prototype/code/micromouse_prototype.ino
@@ -2,62 +2,43 @@
 #include <VL6180X.h>
 #include <Wire.h>
 
-/*This is the code for the initial micromouse prototype
- *
- *
- *
-*/
+/*
+ * This is the code for the initial micromouse prototype
+ */
 
 
-
-
-//motor A connected between A01 and A02
-//motor B connected between B01 and B02
-
+//Motors setup
 int STBY = 9; //standby
-
 //Motor A
 int PWMA = 30; //Speed control
 int AIN1 = 10; //Direction
 int AIN2 = 11; //Direction
-
 //Motor B
 int PWMB = 4; //Speed control
 int BIN1 = 6; //Direction
 int BIN2 = 5; //Direction
 
-
-
-/**
-   IR Sensor
-*/
-
-// IR Sensor Definitions
-VL6180X sensor1;                                     // Sensor that measures from Servo to plate
-VL6180X sensor2;
+// IR Sensor setup
+VL6180X sensor1;  //left IR
+VL6180X sensor2;  //right IR
 int sensor1_pin = 3, sensor2_pin = 2;    // Digital Pins to set GPI00 High or Low.
 
-/**
-   End IR declaration
-*/
-
-
-/**
-   Encoders
-*/
-
+//Encoder setup
 Encoder knobLeft(33, 34);
 Encoder knobRight(7, 8);
 
-/**
-   End Encoder declaration
-*/
+//Global vars
 
-void setup() {
+long positionLeft  = -999;
+long positionRight = -999;
+double ir_difference;
+int right_rotations;
+int left_rotations;
 
-  /**
-     Motor controller
-  */
+
+void setup()
+{
+
   pinMode(STBY, OUTPUT);
 
   pinMode(PWMA, OUTPUT);
@@ -67,12 +48,7 @@ void setup() {
   pinMode(PWMB, OUTPUT);
   pinMode(BIN1, OUTPUT);
   pinMode(BIN2, OUTPUT);
-  /**
-     End motor controller setup code
-  */
-  /**
-     IR Sensor setup
-  */
+
   Wire.setSDA(8);
   Wire.setSCL(7);
 
@@ -91,7 +67,6 @@ void setup() {
   sensor1.setTimeout(500);                          // Timeout time of sensor 0.5 seconds.
   sensor1.setAddress(0x30);                         // Set I2C address as for memory allocation as 0x30.
 
-
   // Sensor 2:
   digitalWrite(sensor2_pin, HIGH);                  // Switch on IR sensor 2.
   delay(50);                                        // 50ms second delay for sensor to come out of standby.
@@ -100,102 +75,102 @@ void setup() {
   sensor2.setTimeout(500);                          // Timeout time of sensor 0.5 seconds.
   sensor2.setAddress(0x31);                         // Set I2C address as for memory allocation as 0x31
 
-
   while (!Serial);                                  // Leonardo: wait for serial monitor
   Serial.println("\nI2C Scanner");                  // Set I2C adress as for memory allocation as 0x56.
 
+  delay(10);
+}
 
-  delay(1000);
-  /**
-     End IR Sensor setup
-  */
+
+void loop()
+{
+  calc_dist_diff();
 
 }
 
-/**
-   Encoders
+
+/*
+================IR FUNCTIONS================
 */
-long positionLeft  = -999;
-long positionRight = -999;
+//Subtract left IR distance from right IR distance
+double calc_dist_diff()
+{
+  double distLeft = getDistanceRight();  //left
+  double distRight = getDistanceLeft();  //Right
 
-void loop() {
-
-  readEncoder();
-
-  move(1, 128, 1);      //motor 1, full speed, left
-  //move(2, 255, 1);    //motor 2, full speed, left
-  //move(1, 255, 2);    //motor 1, full speed, right?
-
-  readEncoder();
-
-  delay(1000);          //go for 1 second
-  stop();               //stop
-  delay(250);           //hold for 250ms until move again
-
-  move(1, 128, 0);      //motor 1, half speed, right
-
-  //move(2, 128, 0);    //motor 2, half speed, right
-
-  readEncoder();
-
-  delay(1000);
-  stop();
-  delay(250);
+  ir_difference = (distRight - distLeft);
 }
 
-void moveTowardsWall() {
-  
-}
-
-double getDistanceA() {
+//Reads value from right IR and converts to inches
+double getDistanceRight()
+{
   double distanceToReturn;
-  //Get Distance and report in mm
-  //Serial.print("Distance measured (in) = ");
-  //Serial.println( sensor1.readRangeSingle()/25.4 );
-
   distanceToReturn = sensor1.readRangeSingle() / 25.4;
-
-  delay(500);
+  delay(10);
+  
   return distanceToReturn;
 }
 
-double getDistanceB() {
+//Read vlue from left IR and converts to inches
+double getDistanceLeft()
+{
   double distanceToReturn;
-
   distanceToReturn = sensor2.readRangeSingle() / 25.4;
-
-  //Get Distance and report in mm
-  //Serial.print("Distance measured (in) = ");
-  //Serial.println( sensor2.readRangeSingle()/25.4 );
-
-  delay(500);
+  delay(10);
+  
   return distanceToReturn;
 }
+/*
+================END IR FUNCTIONS================
+*/
 
-void readEncoder() {
-  long newLeft, newRight;
-  newLeft = knobLeft.read();
+
+
+
+
+/*
+================ENCODER FUNCTIONS================
+*/
+//Counts rotation of right Encoder
+void readRightEncoder()
+{
+  long newRight;
   newRight = knobRight.read();
-  if (newLeft != positionLeft || newRight != positionRight) {
-    Serial.print("Left = ");
-    Serial.print(newLeft);
+  if (newRight != positionRight)
+  {
     Serial.print(", Right = ");
     Serial.print(newRight);
     Serial.println();
-    positionLeft = newLeft;
     positionRight = newRight;
-  }
-  // if a character is sent from the serial monitor,
-  // reset both back to zero.
-  if (Serial.available()) {
-    Serial.read();
-    Serial.println("Reset both knobs to zero");
-    knobLeft.write(0);
-    knobRight.write(0);
   }
 }
 
-void move(int motor, int speed, int direction) {
+//Counts rotation of right Encoder
+void readLeftEncoder()
+{
+  long newLeft;
+  newLeft = knobLeft.read();
+  if (newLEft != positionLeft)
+  {
+    Serial.print(", Right = ");
+    Serial.print(newLeft);
+    Serial.println();
+    positionLeft = newLeft;
+  }
+}
+/*
+================END ENCODER FUNCTIONS================
+*/
+
+
+
+
+
+/*
+================MOTOR FUNCTIONS================
+*/
+void move(int motor, int speed, int direction)
+{
   //Move specific motor at speed and direction
   //motor: 0 for B 1 for A
   //speed: 0 is off, and 255 is full speed
@@ -206,23 +181,31 @@ void move(int motor, int speed, int direction) {
   boolean inPin1 = LOW;
   boolean inPin2 = HIGH;
 
-  if (direction == 1) {
+  if (direction == 1)
+  {
     inPin1 = HIGH;
     inPin2 = LOW;
   }
 
-  if (motor == 1) {
+  if (motor == 1)
+  {
     digitalWrite(AIN1, inPin1);
     digitalWrite(AIN2, inPin2);
     analogWrite(PWMA, speed);
-  } else {
+  }
+  else
+  {
     digitalWrite(BIN1, inPin1);
     digitalWrite(BIN2, inPin2);
     analogWrite(PWMB, speed);
   }
 }
 
-void stop() {
+void stop()
+{
   //enable standby
   digitalWrite(STBY, LOW);
 }
+/*
+================END MOTOR FUNCTIONS================
+*/
